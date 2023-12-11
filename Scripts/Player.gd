c703e144a76313d1ca15bf7831d334bc0d8ee802
shofiqul <acms@acms.acms>
2023-12-11 14:56:30 +0600

    added player animation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,7 +8,7 @@ const lerpValue = -40;
 const jumpTerminationMultiplier = 3;
 
 func _physics_process(delta):
-	var moveVector = getMovementVector();
+	var moveVector = get_movement_vector();
 	
 	velocity.x += moveVector.x * horizontalAcceleration * delta;
 	
@@ -26,9 +26,39 @@ func _physics_process(delta):
 		velocity.y += gravity * delta;
 	
 	move_and_slide();
+	update_animation();
 	
-func getMovementVector():
+func get_movement_vector():
 	var moveVector = Vector2.ZERO;
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0;
 	return moveVector;
+
+func update_animation():
+	var falling = false;
+	var jumping = true;
+	var moveVector = get_movement_vector();
+	
+	if (velocity.y < 0):
+		jumping = true;
+		falling = false;
+	elif (velocity.y > 0):
+		falling = true;
+		jumping = false;
+		
+	if (is_on_floor()):
+		jumping = false;
+		falling = false;
+	
+	if (!falling && !jumping && moveVector.x != 0):
+		$AnimatedSprite2D.play("run");
+	elif (jumping && !falling):
+		$AnimatedSprite2D.play("jump");
+	elif (falling && !jumping):
+		$AnimatedSprite2D.play("fall");
+	elif (!falling && !jumping && moveVector.x == 0):
+		$AnimatedSprite2D.play("idle");
+
+	if (moveVector.x != 0):
+		$AnimatedSprite2D.flip_h = true if moveVector.x < 0 else false;
+
