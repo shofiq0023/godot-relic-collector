6eb2fcb11ea1025c920d0e7bf4e4d092b6cab52f
shofiq <shofiqulislam0023@gmail.com>
2023-12-29 22:53:14 +0600

    added ledge grab

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,48 +1,94 @@
 extends CharacterBody2D
 
 const gravity = 1000.0;
-var maxHorizontalSpeed = 120.0;
 const horizontalAcceleration = 1300;
 const jumpVelocity = -330.0;
 const lerpValue = -40;
 const jumpTerminationMultiplier = 3;
 
+var maxHorizontalSpeed = 120.0;
+var ledgeGrabbing = false;
+
+@onready var rayUp = $RayUp;
+@onready var rayDown = $RayDwn;
+@onready var rayGround = $RayGround;
+@onready var anim = $AnimatedSprite2D;
+@onready var coyoteTimer = $CoyoteTimer;
+
+
 func _physics_process(delta):
 	var moveVector = get_movement_vector();
 	
-	# Character movement acceleration
+	## Character movement acceleration
 	velocity.x += moveVector.x * horizontalAcceleration * delta;
 	
-	# linear interpolation
+	## linear interpolation
+	# Slowly decrease player's movement
 	if (moveVector.x == 0):
 		velocity.x = lerp(0.0, velocity.x, pow(2, lerpValue * delta));
 	
+	# Limit player movement
 	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed);
 	
-	# Character jump
-	if (moveVector.y < 0 && (is_on_floor() || !$CoyoteTimer.is_stopped())):
+	## Character jump
+	# Player will only jump if its on floor or coyoteTimer is active or grabing a ledge
+	if (moveVector.y < 0 && ((is_on_floor() || !coyoteTimer.is_stopped()) || ledgeGrabbing)):
 		velocity.y = jumpVelocity;
-		$CoyoteTimer.stop();
+		coyoteTimer.stop();
+		ledgeGrabbing = false;
+		rayDown.enabled = false;
 	
-	# Jump termination acceleration
+	# Re-enable the ray cast after lading on ground
+	if (is_on_floor()):
+		rayDown.enabled = true;
+	
+	## Jump termination acceleration
+	# Increase falling speed
 	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
 		velocity.y += gravity * jumpTerminationMultiplier * delta;
+	elif (ledgeGrabbing):
+		# Pause player in mid-air
+		velocity.y = 0.0;
 	else:
 		velocity.y += gravity * delta;
+	
+	handleLedgeDetection();
 	
 	var was_on_floor = is_on_floor();
 	move_and_slide();
 	
 	if (was_on_floor && !is_on_floor()):
-		$CoyoteTimer.start();
+		coyoteTimer.start();
 	
 	update_animation();
+
 
 func get_movement_vector():
 	var moveVector = Vector2.ZERO;
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0;
+	
+	handleLdgDirection(moveVector.x);
+	handleLedgeDetection();
+	
 	return moveVector;
+
+
+func handleLdgDirection(moveVector):
+	if moveVector > 0:
+		rayUp.target_position.x = abs(rayUp.target_position.x);
+		rayDown.target_position.x = abs(rayDown.target_position.x);
+	elif moveVector < 0:
+		rayUp.target_position.x = abs(rayUp.target_position.x) * -1;
+		rayDown.target_position.x = abs(rayDown.target_position.x) * -1;
+
+
+func handleLedgeDetection():
+	if (rayDown.is_colliding() && !rayUp.is_colliding() && !is_on_floor() && !rayGround.is_colliding()):
+		ledgeGrabbing = true;
+	else:
+		ledgeGrabbing = false;
+
 
 func update_animation():
 	var falling = false;
@@ -60,14 +106,21 @@ func update_animation():
 		jumping = false;
 		falling = false;
 	
-	if (!falling && !jumping && moveVector.x != 0):
-		$AnimatedSprite2D.play("run");
+	if (ledgeGrabbing):
+		anim.play("ledge_grab");
+		anim.animation_finished;
+	elif (!falling && !jumping && moveVector.x != 0):
+		anim.play("run");
 	elif (jumping && !falling):
-		$AnimatedSprite2D.play("jump");
-	elif (falling && !jumping):
-		$AnimatedSprite2D.play("fall");
+		anim.play("jump");
+	elif (falling && !jumping && !ledgeGrabbing):
+		anim.play("fall");
 	elif (!falling && !jumping && moveVector.x == 0):
-		$AnimatedSprite2D.play("idle");
-
+		anim.play("idle");
+	
 	if (moveVector.x != 0):
-		$AnimatedSprite2D.flip_h = true if moveVector.x < 0 else false;
+		anim.flip_h = true if moveVector.x < 0 else false;
+
+func _on_animated_sprite_2d_animation_finished():
+	if (anim.animation == "ledge_grab"):
+		anim.frame = 5;
