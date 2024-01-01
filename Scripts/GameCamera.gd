19b5f2658a7bbe529d0c6825b9df989907243db1
shofiq <shofiqulislam0023@gmail.com>
2024-01-01 22:51:24 +0600

    refactored code

--- Scripts/GameCamera.gd
+++ Scripts/GameCamera.gd
@@ -1,19 +1,21 @@
 extends Camera2D
 
-@export var backgroundColor = Color();
-var targetPosition = Vector2.ZERO;
-var lerpValue = -15;
+@export var background_color = Color(); #545454
+var target_position = Vector2.ZERO;
+var lerp_value = -15;
 
 func _ready():
-	RenderingServer.set_default_clear_color(backgroundColor);
+	RenderingServer.set_default_clear_color(background_color);
 
-func _process(delta):
+
+func _physics_process(delta):
 	acquire_target_position();
 	
-	global_position = lerp(targetPosition, global_position, pow(2, lerpValue * delta));
+	global_position = lerp(target_position, global_position, pow(2, lerp_value * delta));
+
 
 func acquire_target_position():
 	var players = get_tree().get_nodes_in_group("player");
 	if (players.size() > 0):
 		var player = players[0];
-		targetPosition = player.global_position;
+		target_position = player.global_position;
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,57 +1,57 @@
 extends CharacterBody2D
 
 const gravity = 1000.0;
-const horizontalAcceleration = 1300;
-const jumpVelocity = -330.0;
-const lerpValue = -40;
-const jumpTerminationMultiplier = 3;
+const horizontal_acceleration = 1300;
+const jump_velocity = -330.0;
+const lerp_value = -40;
+const jump_termination_multiplier = 3;
 
-var maxHorizontalSpeed = 120.0;
-var ledgeGrabbing = false;
+var max_horizontal_speed = 120.0;
+var ledge_grabbing = false;
 
-@onready var rayUp = $RayUp;
-@onready var rayDown = $RayDwn;
-@onready var rayGround = $RayGround;
+@onready var ray_up = $RayUp;
+@onready var ray_down = $RayDwn;
+@onready var ray_ground = $RayGround;
 @onready var anim = $AnimatedSprite2D;
-@onready var coyoteTimer = $CoyoteTimer;
-@onready var rayEnableTimer = $RayEnableTimer;
+@onready var coyote_timer = $CoyoteTimer;
+@onready var ray_enable_timer = $RayEnableTimer;
 
 
 func _physics_process(delta):
 	var moveVector = get_movement_vector();
 	
 	## Character movement acceleration
-	velocity.x += moveVector.x * horizontalAcceleration * delta;
+	velocity.x += moveVector.x * horizontal_acceleration * delta;
 	
 	## linear interpolation
 	# Slowly decrease player's movement
 	if (moveVector.x == 0):
-		velocity.x = lerp(0.0, velocity.x, pow(2, lerpValue * delta));
+		velocity.x = lerp(0.0, velocity.x, pow(2, lerp_value * delta));
 	
 	# Limit player movement
-	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed);
+	velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed);
 	
 	## Character jump
-	# Player will only jump if its on floor or coyoteTimer is active or grabing a ledge
-	if (moveVector.y < 0 && ((is_on_floor() || !coyoteTimer.is_stopped()) || ledgeGrabbing)):
-		velocity.y = jumpVelocity;
-		coyoteTimer.stop();
-		ledgeGrabbing = false;
-		rayDown.enabled = false;
-		rayEnableTimer.start();
+	# Player will only jump if its on floor or coyote_timer is active or grabing a ledge
+	if (moveVector.y < 0 && ((is_on_floor() || !coyote_timer.is_stopped()) || ledge_grabbing)):
+		velocity.y = jump_velocity;
+		coyote_timer.stop();
+		ledge_grabbing = false;
+		ray_down.enabled = false;
+		ray_enable_timer.start();
 	
 	# Re-enable the ray cast after lading on ground
 	if (is_on_floor()):
-		rayDown.enabled = true;
+		ray_down.enabled = true;
 	
 	# Enable ray cast after a certain time
-	enableRayDown();
+	enableRay_down();
 	
 	## Jump termination acceleration
 	# Increase falling speed
 	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
-		velocity.y += gravity * jumpTerminationMultiplier * delta;
-	elif (ledgeGrabbing):
+		velocity.y += gravity * jump_termination_multiplier * delta;
+	elif (ledge_grabbing):
 		# Pause player in mid-air
 		velocity.y = 0.0;
 	else:
@@ -63,15 +63,15 @@ func _physics_process(delta):
 	move_and_slide();
 	
 	if (was_on_floor && !is_on_floor()):
-		coyoteTimer.start();
+		coyote_timer.start();
 	
 	update_animation();
 
 
 # Enable ray cast after 0.25 second
-func enableRayDown():
-	await rayEnableTimer.timeout;
-	rayDown.enabled = true;
+func enableRay_down():
+	await ray_enable_timer.timeout;
+	ray_down.enabled = true;
 
 
 func get_movement_vector():
@@ -87,18 +87,18 @@ func get_movement_vector():
 
 func handleLdgDirection(moveVector):
 	if moveVector > 0:
-		rayUp.target_position.x = abs(rayUp.target_position.x);
-		rayDown.target_position.x = abs(rayDown.target_position.x);
+		ray_up.target_position.x = abs(ray_up.target_position.x);
+		ray_down.target_position.x = abs(ray_down.target_position.x);
 	elif moveVector < 0:
-		rayUp.target_position.x = abs(rayUp.target_position.x) * -1;
-		rayDown.target_position.x = abs(rayDown.target_position.x) * -1;
+		ray_up.target_position.x = abs(ray_up.target_position.x) * -1;
+		ray_down.target_position.x = abs(ray_down.target_position.x) * -1;
 
 
 func handleLedgeDetection():
-	if (rayDown.is_colliding() && !rayUp.is_colliding() && !is_on_floor() && !rayGround.is_colliding()):
-		ledgeGrabbing = true;
+	if (ray_down.is_colliding() && !ray_up.is_colliding() && !is_on_floor() && !ray_ground.is_colliding()):
+		ledge_grabbing = true;
 	else:
-		ledgeGrabbing = false;
+		ledge_grabbing = false;
 
 
 func update_animation():
@@ -117,14 +117,14 @@ func update_animation():
 		jumping = false;
 		falling = false;
 	
-	if (ledgeGrabbing):
+	if (ledge_grabbing):
 		anim.play("ledge_grab");
 		anim.animation_finished;
 	elif (!falling && !jumping && moveVector.x != 0):
 		anim.play("run");
 	elif (jumping && !falling):
 		anim.play("jump");
-	elif (falling && !jumping && !ledgeGrabbing):
+	elif (falling && !jumping && !ledge_grabbing):
 		anim.play("fall");
 	elif (!falling && !jumping && moveVector.x == 0):
 		anim.play("idle");
